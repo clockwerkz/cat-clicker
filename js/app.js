4d92c1c4e9053862c411635fe2a2b805e8928ed8
Carlos <clockwerkz@gmail.com>
2018-07-16 21:28:38 -0700

    version 3, cat list selector in progress

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 const catClicker = document.getElementById('cat-clicker');
 
-
 const cats = [
     { name : 'Cleo',
       clickCount : 0,
+      altText : 'Photo of kitten Cleo with her siblings',
       img : 'img/q-aila-162475-unsplash.jpg'},
       { name : 'Clyde',
       clickCount : 0,
+      altText : 'Photo of Clyde being lazy on the arm of a couch',
       img : 'img/sabri-tuzcu-213760-unsplash.jpg'},
 ];
 
+const catList = document.querySelector('aside .list-of-cats');
+for (let cat of cats) {
+    catList.innerHTML += `<li class='cat-item'>${cat.name}</li>`
+}
 
 for (let cat of cats) {
     let catEntry = document.createElement('div');
     catEntry.innerHTML =  `
             <div class='entry-body'>
-                <img src='${cat.img}' class='cat-img' />
+                <img src='${cat.img}' class='cat-img' alt='${cat.altText}'/>
                 <h2 class='cat-name'>${cat.name}</h2>
             </div> <!-- .entry-body -->
             <h2>Clicked: <span class="click-count">${cat.clickCount}</span>
